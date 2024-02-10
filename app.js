d4d810c427fff4e927d1aedf49fd04933672b905
tahmid-emam <0.1tahmidemam@gmail.com>
2024-02-10 23:26:37 +0600

    files are renamed following standard naming rules

--- app.js
+++ app.js
@@ -2,28 +2,27 @@
 const express=require('express')
 const app=express()
 const mongoose=require('mongoose')
-const router=require('./src/routes/api')
-
+// importing routers
+const userRouter=require('./src/routes/user.router')
+const productRouter=require('./src/routes/product.router')
 // extra packages
 const cors = require('cors')
-const { default: mongoose } = require('mongoose')
 require('dotenv').config()
-
 // using the packages
 app.use(express.json())
 app.use(cors())
 
 // mongoDb connection
 let url = process.env.URL
-
 mongoose.connect(url)
 
 // routing implement
-app.use('api/v1', router)
+app.use('/user/api/v1', userRouter)
+app.use('/product/api/v1', productRouter)
 
 // error routing implement
 app.use("*",(req,res)=>{
-    res.status(404).json({status:"fail",data:"something went wrong"})
+    res.status(404).json({status:"fail",data:"wrong connection"})
 })
 
 module.exports = app;
--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,4 +1,4 @@
-const UserModel = require('../models/UserModel')
+const UserModel = require('../models/userModel')
 
 exports.CreateUser = async (req, res) => {
 
